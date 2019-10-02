b11d2ae410d1c76a198a87e7c81f8d72653f3773
LizNolan <liznolanpdx@gmail.com>
2019-10-02 16:29:09 -0700

    update python drill

--- PythonProjects/pythondrill1.py
+++ PythonProjects/pythondrill1.py
@@ -1,31 +1,27 @@
+
 import os
 import time
 
-filelist= os.listdir(r'C:\Users\15035\Documents\Tech Academy Projects\PythonProjects')
-print(filelist)
 
-fName = 'Liz.txt'
+dirPath = r'C:\Users\15035\Documents\GitHub\The-Tech-Academy-Python-Coding-Projects\PythonProjects'
+filelist= os.listdir(dirPath)
 
-fPath ='C:\\Users\15035\Documents\Tech Academy Projects\PythonProjects\\'
+for filename in filelist:
+   if(filename.endswith(".txt")):
+       print(filename, filename.endswith(".txt"),os.path.join(dirPath, filename),os.path.getmtime(filename))
 
-abPath = os.path.join(fPath, fName)
-print (abPath)
 
-modification_time = os.path.getmtime(r'C:\Users\15035\Documents\Tech Academy Projects\PythonProjects\Liz.txt')
-local_time = time.ctime(modification_time)
-print("Last modification time (Local time):", local_time)
 
-print(r'C:\Users\15035\Documents\Tech Academy Projects\PythonProjects\file10.txt') 
-modification_time = os.path.getmtime(r'C:\Users\15035\Documents\Tech Academy Projects\PythonProjects\file10.txt')
-local_time = time.ctime(modification_time)
-print("Last modification time (Local time):", local_time)
+   
+print()
 
-print(r'C:\Users\15035\Documents\Tech Academy Projects\PythonProjects\file3.txt') 
-modification_time = os.path.getmtime(r'C:\Users\15035\Documents\Tech Academy Projects\PythonProjects\file3.txt')
-local_time = time.ctime(modification_time)
-print("Last modification time (Local time):", local_time)
 
-print(r'C:\Users\15035\Documents\Tech Academy Projects\PythonProjects\Liz2.txt') 
-modification_time = os.path.getmtime(r'C:\Users\15035\Documents\Tech Academy Projects\PythonProjects\Liz2.txt')
-local_time = time.ctime(modification_time)
-print("Last modification time (Local time):", local_time)
+
+
+
+#filename = "file10.txt"
+
+#filePath = os.path.join(dirPath, filename)
+
+#os.path.getmtime(filePath)
+
